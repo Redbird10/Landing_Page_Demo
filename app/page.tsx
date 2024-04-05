53530626cf0e3d3eee5f2bd11904f4640aa4ac2d
Ziyi <rs.x@foxmail.com>
2024-04-05 22:20:07 +0100

    Implement two Title Box

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,16 @@ export default function Home() {
     <div>
       <Header/>
       <main className="">
+        <div className="m-20 text-center">
+          <h1 className="font-bold text-6xl m-10">Pricing & Plans</h1>
+          <p className="text-xl text-gray-500 m-1">With lots of unique blocks, you can easily build a page without coding.</p>
+          <p className="text-xl text-gray-500 m-1">Build your next consultancy website within few minutes.</p>
+        </div>
+        <div className="m-20 text-center">
+          <h1 className="font-bold text-6xl m-10">Check our features</h1>
+          <p className="text-xl text-gray-500 m-1">With lots of unique blocks, you can easily build a page without coding.</p>
+          <p className="text-xl text-gray-500 m-1">Build your next consultancy website within few minutes.</p>
+        </div>
       </main>
       <Footer/>
     </div>
